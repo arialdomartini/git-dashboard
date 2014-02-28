681266099351541a3b53eed39e61329c61ad9e60
Arialdo Martini <arialdomartini@gmail.com>
2014-02-28 23:16:48 +0100

    w, dn and an, based on git status, awk and xargs

---
+++ git-dashboard.sh
@@ -0,0 +1,11 @@
+w() {
+    clear &&  ls -l && echo && git branch && echo && git status --short --branch
+}
+
+dn() {
+    git status --short --branch | grep '^.[DM\?]' | head -1 | awk '$1 ~ /[MD]/ {print $2} $1 ~ /\?/ {print "/dev/null " $2}' | xargs git diff -- && w
+}
+
+an() {
+    git status --short --branch | grep '^.[DM\?]' | head -1 | awk '$1 ~ /[M?]/ {print "add " $2} $1 ~ /D/ {print "rm " $2}' | xargs git && w
+}
